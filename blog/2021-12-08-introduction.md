a3a5c2befb37704b3170d87f2ea416f318912542
EmmaKidney <ekidney@redhat.com>
2021-12-08 11:45:18 +0000

    initialise

---
+++ blog/2021-12-08-introduction.md
@@ -0,0 +1,15 @@
+---
+slug: introduction
+title: Introduction
+authors:
+  name: Emma Kidney
+  title: Intern @ Red Hat CPE
+  image_url: https://github.com/ekidneyrh.png
+tags: [intro, docusaurus]
+---
+
+Hello! My name is Emma Kidney. I am currently an intern for the CPE (Community Platform Engineering) team at Red Hat.
+
+I have worked on a few things since joining the team, but my main interest lies within UX Design. I recently connected with Mo Duffy, Senior Principal Interaction Designer, to get involved in the design universe of Fedora. I was encouraged to start this blog to track my progress and get involved in the community.
+
+For some background on myself: I completed an honours BA in Design in 2019 and I am currently at the end of my post-grad degree in Computer Science.
---
+++ docusaurus.config.js
@@ -0,0 +1,116 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'ekidney blog',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'ekidneyrh', // Usually your GitHub org/user name.
+  projectName: 'ekidneyblog', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: false,
+        blog: {
+          blogTitle: 'A blog by ekidney',
+          blogDescription: 'Documenting experience as a design intern for Fedora.',
+          path: './blog',
+          editUrl: 'https://github.com/',
+          showReadingTime: true,
+          routeBasePath: '/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'ekidneys blog',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/profile.jpeg',
+        },
+        items: [
+         // {
+          //  type: 'doc',
+           // docId: 'intro',
+           // position: 'left',
+           // label: 'Tutorial',
+         // },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
