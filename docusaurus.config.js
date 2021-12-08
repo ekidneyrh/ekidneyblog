6debcdc1d8c1405fddcf1604673d6fa85a4066c4
EmmaKidney <ekidney@redhat.com>
2021-12-08 12:03:19 +0000

    fix paths

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,15 +65,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
@@ -93,10 +84,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
